d7aea8109102c1b2fc5f0c4e18df1dee318af30c
beckert26 <31671586+beckert26@users.noreply.github.com>
2020-04-12 00:09:47 -0500

    Add files via upload

---
+++ Assignments/sprint-3/CoronaCases/CoronaCases/CoronaCounties.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoronaCases
+{
+    class CoronaCounties
+    {
+        public CoronaCounties(DateTime date, string county, string state, int fips, int cases, int deaths)
+        {
+            this.Date = date;
+            this.County = county;
+            this.State = state;
+            this.Fips = fips;
+            this.Cases = cases;
+            this.Deaths = deaths;
+        }
+        public DateTime Date { get; set; }
+        public string County { get; set; }
+        public string State { get; set; }
+        public int Fips { get; set; }
+        public int Cases { get; set; }
+        public int Deaths { get; set; }
+    }
+}
